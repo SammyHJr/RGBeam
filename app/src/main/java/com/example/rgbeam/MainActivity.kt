600c43daca4e818e061370fcc68bbe3618f56e41
Sammy H. jr <132605016+SammyHJr@users.noreply.github.com>
2025-05-29 23:38:14 +0200

    UPdate

--- app/src/main/java/com/example/rgbeam/MainActivity.kt
+++ app/src/main/java/com/example/rgbeam/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             RGBeamTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Sam",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     RGBeamTheme {
-        Greeting("Android")
+        Greeting("Sam")
     }
 }
